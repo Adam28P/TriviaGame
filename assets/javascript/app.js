f64899c5901b9e15222f4bc2a63808a9a9a10a3d
Adam P <adamparalovos@hotmail.com>
2018-07-30 20:42:38 -0400

    Added trivia questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+
+    var questions = [{
+            question: "What is the name of the town where the Simpsons live?",
+            answerArray: [{
+                answer: "Shelbyville",
+                val: 0
+            }, {
+                answer: "Springfield",
+                val: 1
+            }, {
+                answer: "Toronto",
+                val: 0
+            },  {
+                answer: "Seinfield",
+                val: 0
+            }, ],
+            response: "The Simpsons live in Springfield!"
+        },  {
+            question: "What is the name of the Simpsons' next door neighbour?",
+            answerArray: [{
+                answer: "Ned Flanders",
+                val: 1
+            }, {
+                answer: "Moe Szyslak",
+                val: 0
+            }, {
+                answer: "Seymour Skinner",
+                val: 0
+            }, {
+                answer: "Barney Gumble",
+                val: 0
+            }, ],
+            response: "The name of the Simpsons' next door neighbour is Ned Flanders!"
+        }, {
+            question: "How old is Bart?",
+            answerArray: [{
+                answer: "9",
+                val: 0
+            }, {
+                answer: "12",
+                val: 0
+            }, {
+                answer: "14",
+                val: 0
+            }, {
+                answer: "10",
+                val: 1
+            }, ],
+            response: "Bart is 10 years old!"
+        },  {
+            question: "Who runs the Kwik-E-Mart?",
+            answerArray: [{
+                answer: "Homer",
+                val: 0
+            }, {
+                answer: "Apu",
+                val: 1
+            }, {
+                answer: "Lenny",
+                val: 0
+            }, {
+                answer: "Carl",
+                val: 0
+            }],
+            response: "Apu runs the Kwik-E-Mart!"
+        },  {
+            question: "Who inventd the Flaming Moe?",
+            answerArray: [{
+                answer: "Homer",
+                val:   1
+            }, {
+                answer: "Marge",
+                val: 0
+            }, {
+                answer: "Moe",
+                val: 0
+            }, {
+                answer: "Bart",
+                val: 0
+            }, ],
+            response: "Homer invented the Flaming Moe!"
+        },  {
+            question: "Who continuously tries to kill Bart?",
+            answerArray: [{
+                answer: "Sideshow Mel",
+                val: 0
+            }, {
+                answer: "Krusty the Clown",
+                val: 0
+            }, {
+                answer: "Sideshow Bob",
+                val: 1,
+            }, {
+                answer: "Ned Flanders",
+                val: 0,
+            }, ],
+            response: "Sideshow Bob continuosuly tries to kill Bart!"
+        },  {
+            question: "What is Marge's maiden name?",
+            answerArray: [{
+                answer: "Flanders",
+                val: 0
+            }, {
+                answer: "Simpson",
+                val: 0
+            }, {
+                answer: "Smith",
+                val: 0
+            }, {
+                answer: "Bouvier",
+                val: 1
+            }, ],
+            response: "Marge's maiden name is Bouvier!"
+        },
+      ];
+
+});
